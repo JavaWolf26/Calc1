64a205bf32fac6603569896ed85d0ef4746bae18
JavaWolf26 <javawolf26@gmail.com>
2021-07-13 16:04:59 +0700

    1

--- src/pac1/Calc2.java
+++ src/pac1/Calc2.java
@@ -45,39 +45,6 @@ public class Calc2 {
 				}
 			}else{System.out.println("Neobhodimie 4isla ot 0 do 9");}
 		}else{System.out.println("Neobhodimie 4isla ot 0 do 9");}
-		
-		/*
-		char a1 = (char) System.in.read ();
-		char arif = (char) System.in.read ();
-	    char a2 = (char) System.in.read ();
-	    int result;
-	    int i1 = Integer.parseInt(String.valueOf(a1));
-	    int i2 = Integer.parseInt(String.valueOf(a2));
-	    if(arif=='+'){
-	    	result=i1+i2;
-	    	System.out.print(result);
-	    }
-	    if(arif=='*'){
-	    	result=i1*i2;
-	    	System.out.print(result);
-	    }
-	    if(arif=='-'){
-	    	result=i1-i2;
-	    	System.out.print(result);
-	    }
-	    if(arif=='/'){
-	    	try {
-	    		result=i1/i2;
-				if (i1 % i2 == 0) {
-				System.out.print(result);
-				} else {
-					System.out.print(result + "; ");
-					System.out.print("Ostatok: " + i1 % i2);}
-			}catch(ArithmeticException e) {
-				System.out.println("Delit' na 0 nelza!");
-			}
-		}
-		*/
-	    in.close();
+		in.close();
 	}
 }
